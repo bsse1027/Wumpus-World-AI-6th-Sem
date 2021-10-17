3f8d40a20b5b190128b927c13b85ca5f73221db4
dipSaha3232 <46640470+dipSaha3232@users.noreply.github.com>
2021-10-17 16:10:11 +0600

    Random world added

--- src/Whmpus/GameRunner.java
+++ src/Whmpus/GameRunner.java
@@ -3,15 +3,13 @@ package Whmpus;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class GameRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			World whmpusWorld = new World();
+			World whmpusWorld = new World().getPrespecifiedWorld();
 
 			Agent agent = new Agent(whmpusWorld);
 		
@@ -20,11 +18,10 @@ public class GameRunner {
 	
 			Desktop d = Desktop.getDesktop();
 			try {
-				//File gameUI = new File("http://127.0.0.1/ww/Wumpus.html");
-				URI u = new URI("http://127.0.0.1/ww/Wumpus.html");
-				d.browse(u);
+				File gameUI = new File("D:\\xampp\\htdocs\\Wumpus.html");
+				d.browse(gameUI.toURI());
 				
-			} catch (IOException | URISyntaxException e) {
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
